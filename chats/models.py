0fbaff6af5e259a823decb073342bb5dde2d1591
Anurag Shakya <officialanuragdev@gmail.com>
2022-12-13 23:28:37 +0530

    Requirements TXT ADDED

--- chats/models.py
+++ chats/models.py
@@ -5,20 +5,20 @@ from accounts.models import User
 
 
 class Conversation(models.Model):
-    user1=models.ForeignKey(User,on_delete=models.CASCADE)
-    user2=models.ForeignKey(User,on_delete=models.CASCADE)
+    user1=models.ForeignKey(User,on_delete=models.CASCADE,related_name="convs")
+    user2=models.ForeignKey(User,on_delete=models.CASCADE,related_name="convs")
     created_at=models.DateTimeField(auto_now_add=True)
     
 class Message(models.Model):
     conversation=models.ForeignKey(Conversation,on_delete=models.CASCADE,related_name="messages")
-    sender=models.ForeignKey(User,on_delete=models.CASCADE)
-    reciever=models.ForeignKey(User,on_delete=models.CASCADE)
+    sender=models.ForeignKey(User,on_delete=models.CASCADE,related_name="sent_messages")
+    reciever=models.ForeignKey(User,on_delete=models.CASCADE,related_name="received_messages")
     message=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
 
     
 class Document(models.Model):
-    message=models.ForeignKey(Message,on_delete=models.CASCADE)
+    message=models.ForeignKey(Message,on_delete=models.CASCADE,related_name="documents")
     document=models.FileField(upload_to="media/documents/")
     created_at=models.DateTimeField(auto_now_add=True)
     
